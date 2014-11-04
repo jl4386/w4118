22a183edaee726eeb742eff4545f52343e9d3380
giddens9527 <liujiaqi0610@gmail.com>
2014-11-04 11:46:47 -0500

    user partially done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,34 @@
 class UsersController < ApplicationController
+	def new 
+		@user = User.new
+	end
 	def show
 		@user = User.find(params[:id])
 	end
 		
+	def create
+		@user=User.new(params[:user])
+		if @user.save
+			redirect_to @user
+		else
+			render 'new'
+		end
+		
+	end
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed"
+	end
+	
+	def edit 
+	end
+
+	def update
+		if@user.update_attributes(params[:user])
+			flahs[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
-  attr_accessible :Major, :Year_of_graduation, :date_of_birth, :degree, :email, :name, :overall_gpa, :user_id
+	has_secure_password
+  attr_accessible :major, :year_of_graduation, :date_of_birth, :degree, :email, :name, :overall_gpa, :user_id, :password
+
+    # users.password_hash in the database is a :string
+  #before_save :create_remember_token
+   private
+        def create_remember_token
+          self.remember_token = SecureRandom.urlsafe_base64
+       end
+      
 end
