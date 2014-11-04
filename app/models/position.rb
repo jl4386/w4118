32faabb324d787e2b2f6f3947d3a416289d63a27
giddens9527 <liujiaqi0610@gmail.com>
2014-11-04 13:26:05 -0500

    type prob solved

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,3 @@
 class Position < ActiveRecord::Base
-  attr_accessible :category, :company_id, :deadline, :position_id, :position_name, :type
+  attr_accessible :category, :company_id, :deadline, :position_id, :position_name, :jobtype
 end
