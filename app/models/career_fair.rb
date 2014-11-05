cef1b86a9d302bc4ebfd5af29e5684604c88cbf5
Ruibo Li <rl2733@columbia.edu>
2014-11-05 15:47:46 -0500

    talbes

--- app/models/career_fair.rb
+++ app/models/career_fair.rb
@@ -1,7 +1,7 @@
 class CareerFair < ActiveRecord::Base
   attr_accessible :activity_id, :fair_id, :person_in_charge
-  has_many :paticipates, :dependent => :destroy
-  has_many :companies, :through => :paticipates
+  has_many :participates, :dependent => :destroy
+  has_many :companies, :through => :participates
   def activity
   	RecruitmentActivity.find_by_activity_id(activity_id)
   end
