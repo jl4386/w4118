b06af007c1a399e2995403718fdf40efb383b173
Ruibo Li <rl2733@columbia.edu>
2014-11-05 01:54:53 -0500

    info

--- app/models/info_session.rb
+++ app/models/info_session.rb
@@ -1,3 +1,6 @@
 class InfoSession < ActiveRecord::Base
   attr_accessible :activity_id, :contact_email, :food, :host_company, :session_id
+  def company
+  	Company.find_by_company_id(host_company)
+  end
 end
