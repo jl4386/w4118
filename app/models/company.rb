a1842886854af73ac4fcc5bb5896a288727a80cc
giddens9527 <liujiaqi0610@gmail.com>
2014-11-05 14:56:13 -0500

    session huh

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ActiveRecord::Base
   attr_accessible :company_id, :company_name, :headquarter_location, :website
+  has_many :paticipates, :dependent => :destroy
+  has_many :career_fair, :through => :paticipates
 end
