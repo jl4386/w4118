e664bd3fac5d9aac812ba10981179335047b1f25
Ruibo Li <rl2733@columbia.edu>
2014-11-05 14:27:51 -0500

    prefers added

--- app/models/position.rb
+++ app/models/position.rb
@@ -2,6 +2,8 @@ class Position < ActiveRecord::Base
   attr_accessible :category, :company_id, :deadline, :position_id, :position_name, :jobtype
   has_many :requires, :dependent => :destroy
   has_many :documents, :through => :requires
+  has_many :prefers, :dependent => :destroy
+  has_many :skills, :through => :prefers
   def company
 	  Company.find_by_company_id(company_id)
   end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,5 @@
 class Skill < ActiveRecord::Base
   attr_accessible :skill_id, :skill_name
+  has_many :prefers
+  has_many :positions, :through => :prefers
 end
