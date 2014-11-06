86f92203d790e12b126775232b0e8a3dec07e352
Ruibo Li <rl2733@columbia.edu>
2014-11-06 11:48:34 -0500

    update profile revised

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,11 +33,21 @@ class UsersController < ApplicationController
 
 	def update
 		@user = User.find(params[:id])
-		if@user.update_attributes(params[:user])
-			sign_in @user
-			redirect_to @user
-		else
-			render 'edit'
-		end
+
+	 	respond_to do |format|
+	      if @user.update_attributes(params[:user])
+	        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+	        format.json { head :no_content }
+	      else
+	        format.html { render action: "edit" }
+	        format.json { render json: @user.errors, status: :unprocessable_entity }
+	      end
+	    end
+		#if @user.update_attributes(params[:user])
+		#	sign_in @user
+		#	redirect_to @user
+		#else
+		#	render 'edit'
+		#end
 	end
 end
